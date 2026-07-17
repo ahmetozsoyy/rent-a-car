4b569bd8a23864fecb985502a44a85f71f1d4239
Ahmet <ozsoyyahmett@gmail.com>
2026-07-17 20:48:17 +0300

    Faz 4 - Adım 3: FluentValidation kuralları yazıldı.

---
+++ RentACar.Application/Validators/CreateReservationRequestValidator.cs
@@ -0,0 +1,21 @@
+using FluentValidation;
+using RentACar.Application.DTOs;
+
+namespace RentACar.Application.Validators;
+
+public class CreateReservationRequestValidator : AbstractValidator<CreateReservationRequest>
+{
+    public CreateReservationRequestValidator()
+    {
+        RuleFor(x => x.UserId).NotEmpty().WithMessage("Kullanıcı ID gereklidir.");
+        RuleFor(x => x.VehicleId).NotEmpty().WithMessage("Araç ID gereklidir.");
+        RuleFor(x => x.PickupLocationId).NotEmpty().WithMessage("Alış lokasyonu gereklidir.");
+        RuleFor(x => x.DropoffLocationId).NotEmpty().WithMessage("Teslim lokasyonu gereklidir.");
+        
+        RuleFor(x => x.StartDate)
+            .GreaterThanOrEqualTo(DateTime.UtcNow.Date).WithMessage("Başlangıç tarihi geçmişte olamaz.");
+            
+        RuleFor(x => x.EndDate)
+            .GreaterThan(x => x.StartDate).WithMessage("Bitiş tarihi başlangıç tarihinden sonra olmalıdır.");
+    }
+}
