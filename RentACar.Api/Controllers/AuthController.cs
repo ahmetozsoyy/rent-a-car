bd45412cefbe48aac1d0a691ed5f53746e2425d2
Ahmet <ozsoyyahmett@gmail.com>
2026-07-17 21:57:29 +0300

    Faz 9 - Adim 6: AuthController olusturuldu

---
+++ RentACar.Api/Controllers/AuthController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using RentACar.Application.DTOs;
+using RentACar.Application.Services;
+
+namespace RentACar.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly IAuthService _authService;
+
+    public AuthController(IAuthService authService)
+    {
+        _authService = authService;
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] RegisterRequest request)
+    {
+        var response = await _authService.RegisterAsync(request);
+        return Ok(response);
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginRequest request)
+    {
+        var response = await _authService.LoginAsync(request);
+        return Ok(response);
+    }
+}
